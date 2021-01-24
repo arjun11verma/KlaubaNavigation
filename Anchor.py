3550f87b7b380ea2da24b969f058169045cb5846
Arjun Verma <averma332@gatech.edu>
2021-01-24 17:19:34 -0500

    Finding (x,y) in terms of R

--- Anchor.py
+++ Anchor.py
@@ -1,6 +1,6 @@
 class anchor:
     def __init__(self, NUM, X_POS, Y_POS, TDOA_BASE):
-        """Gives x/y positions in meters and TDoA in nanoseconds"""
+        """x/y positions in meters and TDoA in nanoseconds"""
         self.NUM = NUM
         self.X_POS = X_POS
         self.Y_POS = Y_POS
--- TDoALocalization.py
+++ TDoALocalization.py
@@ -5,7 +5,19 @@ class TDoALocalization:
     @staticmethod
     def coordinatesInTermsOfR(base_anchor, anchors):
         K = list(map(lambda anchor : anchor.X_POS*anchor.X_POS + anchor.Y_POS*anchor.Y_POS, anchors))
+        base_K = base_anchor.X_POS*base_anchor.X_POS + base_anchor.Y_POS*base_anchor.Y_POS
         anchor_coordinate_differences = list(map(lambda anchor : (anchor.X_POS - base_anchor.X_POS, anchor.Y_POS - base_anchor.Y_POS), anchors))
         R = list(map(lambda anchor : anchor.TDOA_BASE * c, anchors))
+
+        coordinate_difference_matrix = -1 * np.linalg.inv(np.array([[anchor_coordinate_differences[0][0], anchor_coordinate_differences[0][1]], [anchor_coordinate_differences[1][0], anchor_coordinate_differences[1][1]]]))
+        range_difference_matrix = np.array([[R[0]], [R[1]]])
+        range_k_matrix = 0.5 * np.array([[R[0] - K[0] + base_K], [R[1] - K[1] + base_K]])
+
+        r_coefficient = np.diag(coordinate_difference_matrix * range_difference_matrix)
+        constant = np.diag(coordinate_difference_matrix * range_k_matrix)
+
+        print("Coefficient of R: " + str(r_coefficient))
+        print("Constant added: " + str(constant))
+        
         
 
