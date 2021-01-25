b5e5c5ddf482c8aff038cee7c764519a02522efe
Arjun Verma <averma332@gatech.edu>
2021-01-25 14:45:26 -0500

    Another Print Update

--- TDoALocalization.py
+++ TDoALocalization.py
@@ -11,7 +11,7 @@ class TDoALocalization:
 
         coordinate_difference_matrix = -1 * np.linalg.inv(np.array([[anchor_coordinate_differences[0][0], anchor_coordinate_differences[0][1]], [anchor_coordinate_differences[1][0], anchor_coordinate_differences[1][1]]]))
         range_difference_matrix = np.array([[R[0]], [R[1]]])
-        range_k_matrix = 0.5 * np.array([[R[0] - K[0] + self.base_K], [R[1] - K[1] + self.base_K]])
+        range_k_matrix = 0.5 * np.array([[R[0]*R[0] - K[0] + self.base_K], [R[1]*R[1] - K[1] + self.base_K]])
 
         self.r_coefficient = (np.matmul(coordinate_difference_matrix, range_difference_matrix))
         self.constant = (np.matmul(coordinate_difference_matrix, range_k_matrix))
@@ -22,6 +22,9 @@ class TDoALocalization:
         # since r is equal to the distance formula, R(1) = K(1) - 2X(1)x - 2Y(1)y + x^2 + y^2
         if not hasattr(self, 'base_K'):
             self.base_K = base_anchor.X_POS*base_anchor.X_POS + base_anchor.Y_POS*base_anchor.Y_POS
+        
+        if not (hasattr(self, 'r_coefficient') and hasattr(self, 'constant')):
+            raise AttributeError("Please call the coordinates in terms of R method before approximating R")
         
         range_squared_coefficient = 1 - self.r_coefficient[0][0]*self.r_coefficient[0][0] - self.r_coefficient[1][0]*self.r_coefficient[1][0]
         range_coefficient = 2*base_anchor.X_POS*self.r_coefficient[0][0] + 2*base_anchor.Y_POS*self.r_coefficient[1][0] - 2*self.r_coefficient[0][0]*self.constant[0][0] - 2*2*self.r_coefficient[1][0]*self.constant[1][0]
--- TestDriver.py
+++ TestDriver.py
@@ -2,7 +2,7 @@ from Anchor import anchor;
 from TDoALocalization import TDoALocalization;
 
 BASE_ANCHOR = anchor('base', 0, 0, 0)
-ANCHOR_1 = anchor(1, 5, 0, 20)
+ANCHOR_1 = anchor(1, 5, 0, 5)
 ANCHOR_2 = anchor(2, 0, 5, 10)
 
 localization_algorithms = TDoALocalization()
@@ -15,6 +15,8 @@ print("Constant added: " + str(constant))
 r_one, r_two = localization_algorithms.chanHoApproximationOfR(BASE_ANCHOR)
 
 chanho_approximated_r = r_one if r_coefficient[0][0]*r_one + constant[0][0] > 0 and r_coefficient[1][0]*r_one + constant[1][0] > 0 else r_two
+
+print("Approximations of R: " + str(r_one) + ", " + str(r_two))
 
 print("Chan Ho Approximation of (x, y): " + str(r_coefficient[0][0]*chanho_approximated_r + constant[0][0]) + ", " +  str(r_coefficient[1][0]*chanho_approximated_r + constant[1][0]))
 
