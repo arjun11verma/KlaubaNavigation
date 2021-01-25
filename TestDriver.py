3adcfa964cd896d5074546f86a2afcc997bdf255
Arjun Verma <averma332@gatech.edu>
2021-01-25 15:49:21 -0500

    Print formatting

--- TestDriver.py
+++ TestDriver.py
@@ -9,17 +9,17 @@ localization_algorithms = TDoALocalization()
 
 r_coefficient, constant = localization_algorithms.coordinatesInTermsOfR(BASE_ANCHOR, (ANCHOR_1, ANCHOR_2))
 
-print("Coefficient of R: " + str(r_coefficient))
-print("Constant added: " + str(constant))
+print(f'Coefficient of R: {r_coefficient}')
+print(f'Constant added: {constant}')
 
 r_one, r_two = localization_algorithms.chanHoApproximationOfR(BASE_ANCHOR)
 
-print("Approximations of R: " + str(r_one) + ", " + str(r_two))
+print(f'Chan Ho Approximations of R: {r_one}, {r_two}')
 
 chanho_approximated_r = r_one if r_one > 0 else r_two
 
 if(chanho_approximated_r > 0):
-    print(f'Chan Ho Approximation of (x, y): {round(r_coefficient[0][0]*chanho_approximated_r + constant[0][0], 3)}, {round(r_coefficient[1][0]*chanho_approximated_r + constant[1][0], 3)}')
+    print(f'Chan Ho Approximation of (x, y): ({round(r_coefficient[0][0]*chanho_approximated_r + constant[0][0], 3)}, {round(r_coefficient[1][0]*chanho_approximated_r + constant[1][0], 3)})')
 else:
     print("TDoA values out of range")
 
