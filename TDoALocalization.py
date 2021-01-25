e8681759b05b99360b506dce9744211abb1e264e
Arjun Verma <averma332@gatech.edu>
2021-01-25 14:07:34 -0500

    Implementation of Chan Ho approximation of R

--- TDoALocalization.py
+++ TDoALocalization.py
@@ -1,23 +1,35 @@
 import numpy as np;
+import math;
 
 c = 0.299792458 # m/ns
 class TDoALocalization:
-    @staticmethod
-    def coordinatesInTermsOfR(base_anchor, anchors):
+    def coordinatesInTermsOfR(self, base_anchor, anchors):
+        self.base_K = base_anchor.X_POS*base_anchor.X_POS + base_anchor.Y_POS*base_anchor.Y_POS
         K = list(map(lambda anchor : anchor.X_POS*anchor.X_POS + anchor.Y_POS*anchor.Y_POS, anchors))
-        base_K = base_anchor.X_POS*base_anchor.X_POS + base_anchor.Y_POS*base_anchor.Y_POS
         anchor_coordinate_differences = list(map(lambda anchor : (anchor.X_POS - base_anchor.X_POS, anchor.Y_POS - base_anchor.Y_POS), anchors))
         R = list(map(lambda anchor : anchor.TDOA_BASE * c, anchors))
 
         coordinate_difference_matrix = -1 * np.linalg.inv(np.array([[anchor_coordinate_differences[0][0], anchor_coordinate_differences[0][1]], [anchor_coordinate_differences[1][0], anchor_coordinate_differences[1][1]]]))
         range_difference_matrix = np.array([[R[0]], [R[1]]])
-        range_k_matrix = 0.5 * np.array([[R[0] - K[0] + base_K], [R[1] - K[1] + base_K]])
+        range_k_matrix = 0.5 * np.array([[R[0] - K[0] + self.base_K], [R[1] - K[1] + self.base_K]])
 
-        r_coefficient = (np.matmul(coordinate_difference_matrix, range_difference_matrix))
-        constant = (np.matmul(coordinate_difference_matrix, range_k_matrix))
+        self.r_coefficient = (np.matmul(coordinate_difference_matrix, range_difference_matrix))
+        self.constant = (np.matmul(coordinate_difference_matrix, range_k_matrix))
 
-        print("Coefficient of R: " + str(r_coefficient))
-        print("Constant added: " + str(constant))
+        return self.r_coefficient, self.constant
+    
+    def chanHoApproximationOfR(self, base_anchor):
+        # since r is equal to the distance formula, R(1) = K(1) - 2X(1)x - 2Y(1)y + x^2 + y^2
+        if not hasattr(self, 'base_K'):
+            self.base_K = base_anchor.X_POS*base_anchor.X_POS + base_anchor.Y_POS*base_anchor.Y_POS
+        
+        range_squared_coefficient = 1 - self.r_coefficient[0][0]*self.r_coefficient[0][0] - self.r_coefficient[1][0]*self.r_coefficient[1][0]
+        range_coefficient = 2*base_anchor.X_POS*self.r_coefficient[0][0] + 2*base_anchor.Y_POS*self.r_coefficient[1][0] - 2*self.r_coefficient[0][0]*self.constant[0][0] - 2*2*self.r_coefficient[1][0]*self.constant[1][0]
+        range_constant = -1*(self.base_K - 2*base_anchor.X_POS*self.constant[0][0] - 2*base_anchor.Y_POS*self.constant[1][0] + self.constant[0][0]*self.constant[0][0] + self.constant[1][0]*self.constant[1][0])
+
+        r_discriminant = math.sqrt(range_coefficient ** 2 - 4*range_squared_coefficient*range_constant)
+
+        return (-1*range_coefficient + r_discriminant)/(2*range_squared_coefficient), (-1*range_coefficient - r_discriminant)/(2*range_squared_coefficient)
         
         
 
--- TestDriver.py
+++ TestDriver.py
@@ -5,4 +5,18 @@ BASE_ANCHOR = anchor('base', 0, 0, 0)
 ANCHOR_1 = anchor(1, 5, 0, 20)
 ANCHOR_2 = anchor(2, 0, 5, 10)
 
-TDoALocalization.coordinatesInTermsOfR(BASE_ANCHOR, (ANCHOR_1, ANCHOR_2))
+localization_algorithms = TDoALocalization()
+
+r_coefficient, constant = localization_algorithms.coordinatesInTermsOfR(BASE_ANCHOR, (ANCHOR_1, ANCHOR_2))
+
+print("Coefficient of R: " + str(r_coefficient))
+print("Constant added: " + str(constant))
+
+r_one, r_two = localization_algorithms.chanHoApproximationOfR(BASE_ANCHOR)
+
+approximated_r = r_one if r_coefficient[0][0]*r_one + constant[0][0] > 0 and r_coefficient[1][0]*r_one + constant[1][0] > 0 else r_two
+
+print("Approximation of (x, y): " + str(r_coefficient[0][0]*approximated_r + constant[0][0]) + ", " +  str(r_coefficient[1][0]*approximated_r + constant[1][0]))
+
+# Harvey = Mihir
+# ChanHo = Arjun
